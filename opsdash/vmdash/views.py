e75de99fbffdcc4e815cfba0a3fb52f613adeb61
Hector Akamine <hakamine@artefactual.com>
2017-11-03 14:55:32 -0700

    Add simple DO API view code using requests

--- opsdash/vmdash/views.py
+++ opsdash/vmdash/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
+import os
+import requests
+
 
 # Create your views here.
+def index(request):
+    do_token = os.getenv("DIGITALOCEAN_API_TOKEN") 
+    if do_token is not None:
+        do_api_url = 'https://api.digitalocean.com/v2/droplets'
+        headers = {'Authorization': 'Bearer {}'.format(do_token)}
+        r = requests.get(do_api_url, headers=headers)
+
+        return HttpResponse("{}".format(r.text))
+
+    else:
+        return HttpResponse("vmdash: DIGITALOCEAN_API_TOKEN needs to be defined")
